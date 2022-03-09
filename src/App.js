10767caf0c556fdf438025766348497fd17af79d
Davide <dadavidenapolitano@gmail.com>
2022-03-09 15:26:39 +0100

    Bozza_completa

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import Top from './pages/Top';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+import React, { useState, useEffect } from 'react';
 import Wrapper from './pages/Wrapper';
+import axios from 'axios';
+
+const url = 'https://react-corso-api.netlify.app/.netlify/functions/gelateria';
 
 
 function App() {
+  const [api, setApi] = useState(url);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const getData = async () => {
+    setIsLoading(true);
+    setIsError(false)
+    try {
+      const response = await axios.get(url);
+      setApi(response.data.data)
+    } catch (error) {
+      setIsError(true)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  } else if (isError) {
+    return <h1>Error</h1>
+  }
+
   return (
-    <div className="App">
-      <section>
-        <div className="top">
-          <Top />
-        </div>
+    <div className="App container">
+      <section className='container-fluid box'>
+        <Wrapper {...api}/>
       </section>
-      <Wrapper />
-      
     </div>
   );
 }
 
 export default App;
+
+
+
--- src/pages/Wrapper.js
+++ src/pages/Wrapper.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Wrapper = () => {
+const Wrapper = (api) => {
+  let [lista] = useState(['all', 'cono', 'coppetta', 'stick']);
+  let [categoria, setCategoria] = useState(api.default);
+
+   const ciccio = (e) => {
+    let { value } = e.target; 
+    if (value === 'all') {
+      setCategoria(api.default)
+    } else {
+      setCategoria(api.default.filter((el) => el.categoria === value))
+    } 
+  }
+  console.log(categoria);
+
+  
   return (
-    <div>Wrapper</div>
+    <>
+      <div className="titolo">
+        <h1>Bella Crema</h1>
+      </div>
+      <div className="navbar">
+          {
+          lista.map((el, index) => {
+              return <button key={index} value={el} onClick={ciccio}>{el}</button>
+            })
+          }
+      </div>
+      {
+        categoria.map((el) => {
+          let { id,img, nome, categoria, prezzo, decrizione: descrizione, } = el;
+          return (
+            <div className="wrapper" key={id}>
+              <div className="foto">
+                <img src={img} alt={nome} />
+              </div>
+              <div className="info">
+                <div className="intestazione">
+                  <div className="cate">
+                      <h5>{nome}</h5>
+                      <h6>{categoria}</h6>            
+                  </div>
+                  <h5>{prezzo}</h5>
+                </div>
+                <div className="descrizione">
+                  <p> {descrizione} </p>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+      
+    </>
   )
 }
 
